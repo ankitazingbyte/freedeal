50bde3c7d8cb9f92fcf597595cb699b5774ae41e
ankita dangi <ankita.dangi@zingbytes.com>
2018-04-25 19:01:27 +0530

    initial commit

--- db/schema.rb
+++ db/schema.rb
@@ -18,7 +18,6 @@ ActiveRecord::Schema.define(version: 20180419110611) do
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "url"
     t.string "url_link"
   end
 
