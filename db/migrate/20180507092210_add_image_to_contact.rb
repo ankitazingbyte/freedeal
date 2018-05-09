7824062005396f72bae7a242cb4bd863c973cdde
ankita dangi <ankita.dangi@zingbytes.com>
2018-05-09 18:55:44 +0530

    show page of conact

--- db/migrate/20180507092210_add_image_to_contact.rb
+++ db/migrate/20180507092210_add_image_to_contact.rb
@@ -1,7 +1,5 @@
 class AddImageToContact < ActiveRecord::Migration[5.1]
   def change
     add_column :contacts, :image, :string
-    add_column :contacts, :latitude, :float
-    add_column :contacts, :longitude, :float
   end
 end
